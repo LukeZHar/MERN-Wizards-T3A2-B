29b267d86defde88d6a45cab79e0806f904d2d21
LukeZHar <15649@coderacademy.edu.au>
2025-02-20 22:20:25 +1000

    Feat:login/register page skeleton

--- src/frontend/src/pages/LoginPage.jsx
+++ src/frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,23 @@
 import react from "react";
-import "../components/Header";
-import "../components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "../styles/login.css";
+
+export default function LoginPage() {
+    return(
+        <div className="login-page">
+            <Header />
+            <main>
+                <h2>Login</h2>
+                <form>
+                    <input type="text" name="username" placeholder="Username" required />
+                    <input type="password" name="password" placeholder="Password" required />
+                    <button type="submit">Login</button>
+                    <button type="google">Login with Google</button>
+                    <p>Don't have an account? <a href="/register">Register</a></p>
+                </form>
+            </main>
+            <Footer />
+        </div>
+    )
+}
