ff38f3e4871b6079144b51df3b46a0cb2508af17
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-20 16:40:08 +1100

    feat: Created a Post Creation form with title, content, priority, and category fields. Implemented Context to allow other components to access and manage posts.

--- src/frontend/src/pages/App.jsx
+++ src/frontend/src/pages/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../styles/App.css'
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import PostCreation from './PostCreation.jsx';
+import { PostProvider } from '../contexts/PostContext.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <PostProvider>
+    <Router>
+      <Routes>
+        <Route path='/add-post' element={<PostCreation /> } />
+      </Routes>
+    </Router>
+    </PostProvider>
   )
 }
 
