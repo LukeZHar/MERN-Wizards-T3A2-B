81a75f6c0af25426ea08d0263dfd21e5ab7f2cc4
she2024 <bullybluedog@hotmail.com>
2025-02-22 21:02:21 +1000

    build: Set up files for styles and Post details page. Work in progress

---
+++ mern-wizards-t3a2-b/src/frontend/pages/PostDeatilsPage.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import datetime from date 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/login.css";
+
