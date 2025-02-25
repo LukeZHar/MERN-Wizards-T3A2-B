e06cb196f4cc54569d050dfbaded39e36f6de037
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-25 15:03:34 +1100

    feat: Working on Post controller

---
+++ src/backend/controllers/PostController.js
@@ -0,0 +1,23 @@
+// Provide CRUD functionalities for Post Model
+
+const { PostModel } = require("../models/PostModel")
+
+// Function to create a post
+async function createPost(title, content, priority, category, authorId, replies, isArchived) {
+    let result = await PostModel.create({
+        // postID and date has default values
+        title: "Task to do",
+        content: "What to do",
+        priority: "high",
+        category: "example1",
+        authorId: new mongoose.Types.ObjectId(),
+        replies: [],
+        isArchived: false
+    })
+
+    return result;
+}
+
+module.exports = {
+    createPost
+}
