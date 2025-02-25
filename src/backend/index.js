222c900ec14c3c2b85c4f05dfbffedc9b0798b55
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-02-25 14:19:11 +1100

    feat: Created a database file to connect to MongoDB

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,13 @@
 // Initialise the server
 const { app } = require("./server.js");
+const { dbConnect } = require("./utils/database.js");
 
 // Get the PORT
 const PORT = process.env.PORT || 8008;
 
 // Listen to incoming traffic
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+    await dbConnect();
+
     console.log(`Server is running on port: ${PORT}`);
 });
