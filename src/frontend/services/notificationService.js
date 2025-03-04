f94d95073b9eeb9ff891784d6ffa702fac5aefd6
LukeZHar <15649@coderacademy.edu.au>
2025-03-04 13:56:36 +1000

    Feat: New route path to home, change in nav on 404

--- src/frontend/services/notificationService.js
+++ src/frontend/services/notificationService.js
@@ -14,15 +14,21 @@ export const createNotification = async (recipientID, relatedPostID, message) =>
 };
 
 // Function to fetch notifications for a specific user
-export const getUserNotifications = async (userId) => {
+export async function getUserNotifications(userId) {
+    console.log('User ID:', userId); // Log the userId
+    if (!userId) {
+        console.error('User ID is undefined.');
+        return; // Exit early if userId is undefined
+    }
+
     try {
         const response = await axios.get(`${API_URL}/api/notifications/${userId}`);
-        return response.data; // Assuming the response is an array of notifications
+        return response.data;
     } catch (error) {
-        console.error("Error fetching notifications:", error);
-        throw new Error("Failed to fetch notifications");
+        console.error('Error fetching notifications:', error);
+        throw new Error('Failed to fetch notifications');
     }
-};
+}
 
 // Function to mark a notification as read
 export const markNotificationAsRead = async (notificationId) => {
