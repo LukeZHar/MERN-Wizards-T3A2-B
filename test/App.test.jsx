c3c7bb222a98ff835c5717d07af6d98d5199fdda
LukeZHar <15649@coderacademy.edu.au>
2025-03-06 13:43:21 +1000

    Fix: Footer styling have a gap either side fix, some spelling fixes

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -6,16 +6,16 @@ import userEvent from "@testing-library/user-event";
 
 //check if rendered content is performing as expected
 
-test("Render the App conponent", () => {
+test("Render the App component", () => {
     render (<App />);
 
     const mainPageHeader = screen.getByText("Vite + React");
     expect (mainPageHeader).toBeInTheDocument();
     });
 
-// render content and simulate inteactions
+// render content and simulate interactions
 
-//test("Render the App component with a button that increses the count", async () => {
+//test("Render the App component with a button that increases the count", async () => {
 //    render(<App />)
 
     // Find button element 
