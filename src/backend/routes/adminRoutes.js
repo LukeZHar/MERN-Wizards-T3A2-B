663150b35fc86581b994e67379c2197cd762fb3a
Jessica Vaz <168509445+Jessicavazm@users.noreply.github.com>
2025-03-09 20:08:27 +1100

    fix: Removing Middlewares for the moment

--- src/backend/routes/adminRoutes.js
+++ src/backend/routes/adminRoutes.js
@@ -6,22 +6,22 @@ const { searchUsers, updateUserRole, deleteUser, searchPosts, updatePostPriority
 const router = express.Router();
 
 // GET /api/admin/users - Retrieve users
-router.get("/users", validateToken, isAdmin, searchUsers);
+router.get("/users", searchUsers);
 
 // PATCH /api/admin/users/:id - Update user role 
-router.patch("/users/:id", validateToken, isAdmin, updateUserRole);
+router.patch("/users/:id", updateUserRole);
 
 // DELETE api/admin/users/:id - Delete a user
-router.delete("/users/:id", validateToken, isAdmin, deleteUser);
+router.delete("/users/:id", deleteUser);
 
 // GET /api/admin/posts - Retrieve posts
-router.get("/posts", validateToken, isAdmin, searchPosts);
+router.get("/posts", searchPosts);
 
 // PATCH /api/admin/post/:id - Update post
-router.patch("/posts/:id", validateToken, isAdmin, updatePostPriority);
+router.patch("/posts/:id", updatePostPriority);
 
 // // DELETE api/admin/post/:id - Delete a post
-router.delete("/posts/:id", validateToken, isAdmin, deletePost);
+router.delete("/posts/:id", deletePost);
 
 // Export the router
 module.exports = router; 
