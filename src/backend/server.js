d50df92f89ec4e3be28a4e848d73076dfcfc0bc0
LukeZHar <15649@coderacademy.edu.au>
2025-03-14 00:06:51 +1000

    Update: To CorsOption for netlify

--- src/backend/server.js
+++ src/backend/server.js
@@ -19,9 +19,19 @@ const { connectDB } = require("./utils/database");
 // Instance of express for configuration
 const app = express();
 
+// CORS configuration
 let corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:5173","http://localhost:8008", "https://reactapp.com","https://mern-wizards-t3a2-b.onrender.com", "http://127.0.0.1:5173", "http://127.0.0.1:5174",  "https://mernwizards-b.xhoo8.mongodb.net/, 'https://a-ticket-a-task-it.netlify.app' "],
-    methods:["GET", "HEAD", "POST","PUT", "PATCH", "DELETE"],
+    origin: [
+        "http://localhost:3000", 
+        "http://localhost:5173",
+        "http://localhost:8008", 
+        "https://reactapp.com",
+        "https://mern-wizards-t3a2-b.onrender.com", 
+        "http://127.0.0.1:5173", 
+        "http://127.0.0.1:5174",  
+        "https://a-ticket-a-task-it.netlify.app" 
+    ],
+    methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200 
